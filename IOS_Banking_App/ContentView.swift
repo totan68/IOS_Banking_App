be685d222e63928d5dc227481fec1c9635d80aea
totan68 <164209696+totan68@users.noreply.github.com>
2024-08-05 18:10:59 +0530

    Initial Commit

---
+++ IOS_Banking_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS_Banking_App
+//
+//  Created by Totan Hazra on 05/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS_Banking_App/IOS_Banking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Banking_AppApp.swift
+//  IOS_Banking_App
+//
+//  Created by Totan Hazra on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Banking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
